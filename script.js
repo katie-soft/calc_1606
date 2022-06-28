81b33beb77d25852e5e72c4f81813614e5c111f6
katie-soft <107564716+katie-soft@users.noreply.github.com>
2022-06-28 22:04:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let buttons = document.querySelectorAll(".button");
 
+buttons.forEach(function(item) {
+	item.addEventListener("click", buttonHandler);
+})
+
+function buttonHandler(event) {
+	event.target.classList.add("no-shadow");
+	
+	setTimeout(function() {
+		event.target.classList.remove("no-shadow");
+	}, 300) 	
+}
